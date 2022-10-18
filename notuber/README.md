73cbfb85b3d0be10d385c45edb266597a048281c
trav-xeno <travisnevins@outlook.com>
2022-10-18 13:49:06 -0400

    fully working only need todo performance

---
+++ notuber/README.md
@@ -0,0 +1,7 @@
+# lab 8
+
+* Identify what aspects of the work have been correctly implemented and what have not.
+* Identify anyone with whom you have collaborated or discussed the lab.
+* how many hours you have spent completing the lab.
+
+## IMPORTANT: impact of performance enhancements. Provide a comparison of the performance of page with vs without optimizations. At bare minimum, provide load times and file sizes.
---
+++ notuber/index.js
@@ -0,0 +1,29 @@
+let map;
+let markers = [];
+const iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+let latLngs = [
+    {id: "mXfkjrFw", lat:42.3453, lng:-71.0464},
+    {id: "nZXB8ZHz", lat:42.3662, lng:-71.0621},
+    {id:"Tkwu74WC" , lat:42.3603 , lng: -71.0547},
+    {id: "5KWpnAJN", lat: 42.3472, lng:-71.0802 },
+    {id: "uf5ZrXYw" , lat:42.3663 , lng:-71.0544 },
+    {id:"VMerzMH8" , lat:42.3542 , lng:-71.0704 }
+];
+function initMap(){
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {  lat: 42.352271, lng: -71.05524200000001},
+    zoom: 14,
+  });
+
+  for( let val of latLngs){
+    const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(val.lat,val.lng),
+        map,
+        icon: "./car.png",
+        title: val.id
+    });
+  }
+}
+
+
+window.initMap = initMap;
